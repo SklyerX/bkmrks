439b1bb653ad852d5dfbe2bed4b256b1da638159
Skyler.Ab <abrishami.bardia@gmail.com>
2024-11-04 15:19:52 -0500

    db setup finished

---
+++ apps/www/src/db/index.ts
@@ -0,0 +1,24 @@
+import { env } from "@/env";
+import * as schema from "./schema";
+import { type PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let pg: ReturnType<typeof postgres>;
+
+if (process.env.NODE_ENV === "production") {
+  pg = postgres(env.DATABASE_URL);
+  db = drizzle(pg, { schema });
+} else {
+  if (!global.db) {
+    pg = postgres(env.DATABASE_URL);
+    global.db = drizzle(pg, { schema });
+  }
+  db = global.db;
+}
+
+export { db, pg };
